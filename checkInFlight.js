340da4acf16bdb0a71bfb019398f682bdf858bd5
julesdmai <julesdmai@gmail.com>
2025-01-13 11:34:10 -0600

    Initiated checkInFlight script

---
+++ checkInFlight.js
@@ -0,0 +1,7 @@
+// This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
+function checkInFlight() {
+
+}
+
+// Execute
+checkInFlight();
