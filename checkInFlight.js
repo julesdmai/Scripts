99c15010a8c6f25b6ad71de58680329970d3be49
julesdmai <julesdmai@gmail.com>
2025-01-14 10:10:46 -0600

    Completed function signature

--- checkInFlight.js
+++ checkInFlight.js
@@ -1,7 +1,12 @@
-// This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
-function checkInFlight() {
+// These are the parameters you - the user - will need to modify
+const confirmationNumber = 'YOUR_CONFIRMATION_NUMBER';
+const firstName = 'YOUR_FIRST_NAME';
+const lastName = 'YOUR_LAST_NAME';
 
+// This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
+function checkInFlight(number, first, last) {
+    return;
 }
 
 // Execute
-checkInFlight();
+checkInFlight(confirmationNumber, firstName, lastName);
