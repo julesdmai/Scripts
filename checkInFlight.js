6a1757ca51897a97ebf4b287eb62e7e195688b65
julesdmai <julesdmai@gmail.com>
2025-01-15 09:20:46 -0800

    Clarified parameters and arguments

--- checkInFlight.js
+++ checkInFlight.js
@@ -1,12 +1,12 @@
 // These are the parameters you - the user - will need to modify
-const confirmationNumber = 'YOUR_CONFIRMATION_NUMBER';
-const firstName = 'YOUR_FIRST_NAME';
-const lastName = 'YOUR_LAST_NAME';
+const yourConfirmationNumber = 'YOUR_CONFIRMATION_NUMBER';
+const yourFirstName = 'YOUR_FIRST_NAME';
+const yourLastName = 'YOUR_LAST_NAME';
 
 // This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
-function checkInFlight(number, first, last) {
+function checkInFlight(confNbr, firstName, lastName) {
     return;
 }
 
-// Execute
-checkInFlight(confirmationNumber, firstName, lastName);
+// Function invocation
+checkInFlight(yourConfirmationNumber, yourFirstName, yourLastName);
