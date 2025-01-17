04bcf9551b2a9564babd3c72218a799bea7c447d
julesdmai <julesdmai@gmail.com>
2025-01-17 08:39:34 -0800

    Working on function definition, grab elements from form

--- checkInFlight.js
+++ checkInFlight.js
@@ -2,16 +2,21 @@
 const yourConfirmationNumber = 'YOUR_CONFIRMATION_NUMBER';
 const yourFirstName = 'YOUR_FIRST_NAME';
 const yourLastName = 'YOUR_LAST_NAME';
+const yourCheckInTime = new Date('2025-01-01T12:00:00') // Adjust to your check-in time
 
 // This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
-function checkInFlight(confNbr, firstName, lastName) {
+function checkInFlight(confNbr, firstName, lastName, checkInTime) {
     'use strict';
 
     // Wrapper to ensure DOM is loaded
     document.addEventListener('DOMContentLoaded', function() {
         // Fill in the form fields
+        document.getElementById('confirmationNumber').value = confNbr;
+        document.getElementById('passengerFirstName').value = firstName;
+        document.getElementById('passengerLastName').value = lastName;
 
         // Submit the form at a specific time
+        const currentTime = new Date();
 
         // Time check
 
@@ -21,4 +26,4 @@ function checkInFlight(confNbr, firstName, lastName) {
 }
 
 // Function invocation
-checkInFlight(yourConfirmationNumber, yourFirstName, yourLastName);
+checkInFlight(yourConfirmationNumber, yourFirstName, yourLastName, yourCheckInTime);
