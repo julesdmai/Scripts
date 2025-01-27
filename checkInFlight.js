469958123043057e059c048aa1f040df8f6a90a9
julesdmai <julesdmai@gmail.com>
2025-01-27 15:04:23 -0800

    Created helper function for separation of concerns

--- checkInFlight.js
+++ checkInFlight.js
@@ -3,51 +3,55 @@ const yourConfirmationNumber = '12341234';
 const yourFirstName = 'Harry';
 const yourLastName = 'Potter';
 const targetTime = { 
-    hour: 13, 
-    minute: 5, 
-    second: 0,
+    hour: 15, 
+    minute: 0, 
+    second: 30,
     milliseconds: 0
 };
 const yourCheckInTime = new Date();
 yourCheckInTime.setHours(targetTime.hour, targetTime.minute, targetTime.second, targetTime.milliseconds);
 
+
 // This function, when invoked, will check into your flight at a point in time. Intended to be used with a scheduler such as cron or TamperMonkey
 function checkInFlight(confNbr, firstName, lastName, checkInTime) {
-
-    // Wrapper to ensure DOM is loaded
     document.addEventListener('DOMContentLoaded', function() {
-        
-        /*
-        // TODO: Check to see if these elements have even be loaded in yet
-        // TODO: Create a variable for the submit button as well
-
-        const confNumberInput = document.getElementById('confirmationNumber')
-        const firstNameInput = document.getElementById('passengerFirstName')
-        const lastNameInput = document.getElementById('passengerLastName')
-        console.log(confNumberInput);
-        */
-
-        // Fill in the form fields
-        document.getElementById('confirmationNumber').value = confNbr;
-        document.getElementById('passengerFirstName').value = firstName;
-        document.getElementById('passengerLastName').value = lastName;
-
-        // TODO: Recheck to see if appropriate values have been filled in
-        // console.log(confNumberInput);
-
-        // Submit the form at a specific time
+        // Time check
         const currentTime = new Date();
         const timeUntilCheckIn = checkInTime - currentTime;
 
         // Time check
         if (timeUntilCheckIn > 0) {
+            console.log(`Waiting ${timeUntilCheckIn} ms until the target time...`);
             setTimeout(() => {
-                document.querySelector('button[type="submit"]').click();
+                console.log(`Target time ${checkInTime} reached. Initiating autofill.`);
+                autoFillAndSubmit();
                 console.log('Your check in has been completed');
             }, timeUntilCheckIn);
         } else {
             console.log('The check in time has already passed');
             return;
+        }
+
+        // TODO: Function for autofillAndSubmit
+        function autoFillAndSubmit() {
+            // TODO: Check to see if these elements have even be loaded in yet
+            // TODO: Create a variable for the submit button as well
+
+            const confNumberInput = document.getElementById('confirmationNumber')
+            const firstNameInput = document.getElementById('passengerFirstName')
+            const lastNameInput = document.getElementById('passengerLastName')
+            console.log(confNumberInput);
+            const submitButton = document.getElementById('form-mixin--submit-button');
+            
+            // Fill in the form fields
+            confNumberInput.value = confNbr;
+            firstNameInput.value = firstName;
+            lastNameInput.value = lastName;
+            
+            // TODO: Recheck to see if appropriate values have been filled in
+            console.log(confNumberInput);
+            console.log(firstNameInput);
+            console.log(lastNameInput);
         }
     });
 }
