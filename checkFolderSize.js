fa57123b25691f1e98b0d05d1cfd1d6a3de28493
julesdmai <julesdmai@gmail.com>
2025-02-12 14:59:15 -0800

    Initial commit and skeleton for checkFolderSize.js

---
+++ checkFolderSize.js
@@ -0,0 +1,20 @@
+// Purpose
+// The purpose of this script is to target a folder and output to the console the file sizes of the child folders within the targeted folder.
+
+// Libraries
+
+
+// User Input
+const TARGET_FOLDER = '';
+const RUN_IN_PROD_MODE = false;
+
+// Main function and invocation
+function checkFolderSize(targetFolder, isProdMode) {
+    console.log('hello world');
+    return;
+}
+checkFolderSize(TARGET_FOLDER, RUN_IN_PROD_MODE)
+
+
+
+// Notes: 
