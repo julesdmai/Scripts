d5b405aa7911b128800355918079673bf9eaa552
julesdmai <julesdmai@gmail.com>
2025-02-17 09:39:18 -0800

    Clarified purpose

--- syncJpegDeletes.js
+++ syncJpegDeletes.js
@@ -1,11 +1,11 @@
 /* 
 The purpose of this script is to synchronize deletion of Raw-JPEG files.
 Typically, a photographer will take photos in 2 formats: (1) Raw and (2) JPEG
-The JPEG file would be used for initial viewing, culling, and sharing.
-The RAW file would be used for maximum fidelity allowing for deeper edits.
+The JPEG file would be used for initial viewing, culling, and initial sharing.
+The RAW file would be used for maximum image fidelity allowing for deeper edits - portfolio, deliverables, client work.
 
-When a photographer reviews their images, they will typically look through the JPEG files which are lighter weight allowing for quicker viewing.
-However, when the user deletes the JPEG file (culling the photo), the RAW file still remains.
+When a photographer reviews their images, they will typically look through the JPEG files which are lighter weight.
+However, when the user deletes a JPEG file (culling the photo), the matching pair RAW file still remains.
 The purpose of this script is to check if the JPEG has been deleted, then go ahead and delete the corresponding raw file. 
 */
 
