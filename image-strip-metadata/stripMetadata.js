b2e6d4a5f1f99d3577abd7c40972f48e57aa9892
julesdmai <julesdmai@gmail.com>
2025-03-12 10:55:57 -0700

    Function comments

---
+++ image-strip-metadata/stripMetadata.js
@@ -0,0 +1,13 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require ('path');
+
+// Point at file paths
+
+
+// Check
+
+
+// Process the image: remove the metadata and save it with a new name
+
+
