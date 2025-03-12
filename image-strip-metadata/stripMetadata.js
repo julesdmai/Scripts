249b776390611b81f41b7435cc9222b03ceef238
julesdmai <julesdmai@gmail.com>
2025-03-12 11:22:23 -0700

    Naming file to current date time

--- image-strip-metadata/stripMetadata.js
+++ image-strip-metadata/stripMetadata.js
@@ -1,35 +1,39 @@
-const sharp = require('sharp');
-const fs = require('fs');
-const path = require ('path');
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
 
 // Point at file paths
-const inputPath = '';
+const inputPath = "";
 
 // Helper function to get current date and time in YYYYMMDD-HHMMSS
 const getCurrentDateTime = () => {
-    const now = new Date();
-    const date = now.toISOString().slice(0,10).replace(/-/g, '');
-    const time = now.toTimeString().slice(0,10).replace(/:/g, '');
-    return `${date}-${time}`;
-}
+  const now = new Date();
+  const date = now.toISOString().slice(0, 10).replace(/-/g, "");
+  const time = now.toTimeString().slice(0, 10).replace(/:/g, "");
+  return `${date}-${time}`;
+};
 
-function stripMetadata(inputPath, outputName) {
-    // Check
-    if (!fs.existsSync(inputPath)) {
-        console.error('Input file does not exist: ', inputPath);
-        return;
-    }
-    // Process the image: remove the metadata and save it with a new name
-    sharp(inputPath)
-        .withMetadata({ exif: false })
-        .toFile(outputName, (err, info) => {
-            if (err) {
-                console.error('Error processing image: ', err);
-            } else {
-                console.log('Image processed and saved');
-            }
-        })
+function stripMetadata(inputPath) {
+  // Check
+  if (!fs.existsSync(inputPath)) {
+    console.error("Input file does not exist: ", inputPath);
+    return;
+  }
 
+  // Generates output file name and path from inputPath
+  const directory = path.dirname(inputPath);
+  const outputName = "CL" + getCurrentDateTime() + ".jpg";
+  const outputPath = path.join(directory, outputName);
+
+  // Process the image: remove the metadata and save it with a new name
+  sharp(inputPath)
+    .withMetadata({ exif: false })
+    .toFile(outputName, (err) => {
+      if (err) {
+        console.error("Error processing image: ", err);
+      }
+      console.log("Image processed successfully");
+    });
 }
-stripMetadata(inputPath, outputName);
-console.log(`Metadata has been stripped from file ${outputName}`);
+stripMetadata(inputPath);
+console.log(`Metadata has been stripped from file saved as ${outputName}`);
