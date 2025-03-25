3955266d9939f0e0fc6a0ed45ac031c717dede52
julesdmai <julesdmai@gmail.com>
2025-03-25 11:45:02 -0700

    Removed node modules

---
+++ csv-reader/index.js
@@ -0,0 +1,15 @@
+console.log('---- printData.js starting... ----')
+
+const fs = require("fs");
+const parse = require("csv-parse");
+
+const target = ""; // Source to CSV file
+
+function printData(source) {
+  const outputStr = "";
+  return outputStr;
+}
+const records = printData(target);
+console.log('Records: ', records);
+
+console.log('---- printData.js finished executing ----')
