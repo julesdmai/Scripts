19212f4be9d3b088bf742c75f7b83336d5809661
julesdmai <julesdmai@gmail.com>
2025-04-23 11:06:01 -0700

    Added parcel

--- csv-reader/src/index.js
+++ csv-reader/src/index.js
@@ -1,44 +1,50 @@
-// require * as d3 from "d3";
+import * as d3 from 'd3';
 
-console.log("---- printData.js starting... ----");
-
-const fs = require("fs");
-const parse = require("csv-parse/sync");
-
-const target = "../assets/test.csv"; // User inputs path to CSV file
-
-function printData(source) {
-  // Obtain handle on target
-  const fileContent = fs.readFileSync(source);
-
-  // Obtain records in array of objects with columns as keys
-  const records = parse.parse(fileContent, {
-    columns: true,
-    skip_empty_lines: true,
+// Load and parse CSV
+d3.csv('../assets/test.csv').then(data => {
+  // Convert string numbers to actual numbers
+  data.forEach(d => {
+    d['Height_(cm)'] = +d['Height_(cm)'];
   });
-  console.log("pre-filter column count: ", Object.keys(records[0]).length);
 
-  // Filter out records where key is empty string ('')
-  const cleanedRecords = records.map((row) => {
-    // Convert each row's object into tuples ie. [key, value] pairs
-    const entries = Object.entries(row);
-    const filtered = entries.filter(([key, val]) => key !== "");
+  drawBarChart(data);
+});
 
-    // Convert filtered back into an object
-    const cleanedRow = Object.fromEntries(filtered);
-    return cleanedRow;
-  });
-  console.log(
-    "post-filter column count: ",
-    Object.keys(cleanedRecords[0]).length
-  );
-  return cleanedRecords;
+function drawBarChart(data) {
+  const svg = d3.select('svg');
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
+  const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.Name))
+    .range([0, innerWidth])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d['Height_(cm)'])])
+    .nice()
+    .range([innerHeight, 0]);
+
+  g.append('g')
+    .call(d3.axisLeft(y));
+
+  g.append('g')
+    .call(d3.axisBottom(x))
+    .attr('transform', `translate(0,${innerHeight})`);
+
+  g.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.Name))
+    .attr('y', d => y(d['Height_(cm)']))
+    .attr('width', x.bandwidth())
+    .attr('height', d => innerHeight - y(d['Height_(cm)']));
 }
-// Function invocation
-const outputRecords = printData(target);
-console.log("Records: ", outputRecords);
-console.log("---- printData.js finished executing ----");
-
-// TODO: Render to D3 here
-
-console.log("Finished rendering D3")
