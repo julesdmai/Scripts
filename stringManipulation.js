98534ff97eab21d8447f838d61472562f17269a2
julesdmai <julesdmai@gmail.com>
2025-05-05 12:51:48 -0700

    Setup function signature for manipulateString

---
+++ stringManipulation.js
@@ -0,0 +1,10 @@
+const INPUT_TEXT = '';
+
+function manipulateString(text) {
+    console.log('manipulateString running');
+
+    // Code goes here
+
+    console.log('manipulateString finished executing');
+}
+manipulateString(INPUT_TEXT);
