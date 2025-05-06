4ab9b36a20209e10083ca6ade15a3f4eb788fbc3
julesdmai <julesdmai@gmail.com>
2025-05-06 09:25:32 -0700

    Modifying minutesElapsed to only take one parameter - the start time

--- minutesElapsed.js
+++ minutesElapsed.js
@@ -1,16 +1,22 @@
 const INPUT_START_TIME = new Date("2025-05-05T08:30:00.000Z").toISOString();
 const INPUT_END_TIME = new Date("2025-05-05T09:45:00.000Z").toISOString();
 
-// const isoStart = "2025-05-05T08:30:00.000Z";
-// const isoEnd = "2025-05-05T09:45:00.000Z";
-
-function minutesElapsed(startTime, endTime) {
+function minutesElapsed(isoStartTime, isoEndTime) {
   console.log("minutesElapsed running");
 
   // Code goes here
-  console.log("startTime: ", startTime);
-  console.log("endTime:   ", endTime);
+  console.log("startTime: ", isoStartTime);
+  console.log("endTime:   ", isoEndTime);
 
-  console.log("minutesElapsed finished executing");
+  const startTimeObj = new Date(isoStartTime);
+  const endTimeObj = new Date(isoEndTime);
+
+  const diffMilliseconds = endTimeObj - startTimeObj;
+  const diffMinutes = diffMilliseconds / (1000 * 60);
+
+  const roundedMinutes = Math.round(diffMinutes);
+
+  console.log("calculated roundedMinutes: ", roundedMinutes);
+  return roundedMinutes;
 }
 minutesElapsed(INPUT_START_TIME, INPUT_END_TIME);
