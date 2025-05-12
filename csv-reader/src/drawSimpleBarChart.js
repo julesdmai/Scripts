0a118bb012fa09b3720aa7a99dab15f64b4ccac1
julesdmai <julesdmai@gmail.com>
2025-05-12 12:38:30 -0700

    Annotated simpleBarChart function

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -3,17 +3,20 @@ function drawSimpleBarChart(data) {
   const width = +svg.attr("width");
   const height = +svg.attr("height");
 
+  // Create the x scale
   const x = d3
     .scaleBand()
     .domain(data.map((d) => d.Name))
     .range([0, width])
     .padding(0.1);
 
+  // Create the y scale
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d["Height_(cm)"])])
     .range([height, 0]);
 
+  // Bind the data and draw the rectangles
   svg
     .selectAll("rect")
     .data(data)
