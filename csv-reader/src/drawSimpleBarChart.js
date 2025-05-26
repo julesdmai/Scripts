26eeb15f3c5ad847dafc15c36e9275ce0b5cab94
julesdmai <julesdmai@gmail.com>
2025-05-26 16:40:21 -0700

    Clarified scaling functions

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -3,14 +3,14 @@ function drawSimpleBarChart(data) {
   const width = +svg.attr("width"); // TODO: Simplify width and height
   const height = +svg.attr("height");
 
-  // Create the x scale
+  // Create the x scale scaling function
   const x = d3
     .scaleBand()
     .domain(data.map((d) => d.Name))
     .range([0, width])
     .padding(0.2);
 
-  // Create the y scale
+  // Create the y scale scaling function
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d["Height_(cm)"])]) // TODO: Simplify field name
