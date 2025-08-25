ebf5a327a6c901da5c4b34b249ffae5f6852e12d
julesdmai <julesdmai@gmail.com>
2025-08-25 08:24:36 -0700

    Added TODO for day

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -43,5 +43,5 @@ function drawSimpleBarChart(data) {
   // TODO: Research API GW
 
   // TODO: Wrap up Cglass pages - Account, Contact
-  // TODO: Cglass - My account skeleton, best practices dark mode,
+  // TODO: Cglass - My account skeleton, best practices dark mode, implement global variables
 }
