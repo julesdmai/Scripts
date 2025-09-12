94689a3ce42fe6204a078426566aa4059498b32b
julesdmai <julesdmai@gmail.com>
2025-09-12 13:17:24 -0700

    TODO for day - filter controls

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -44,5 +44,5 @@ function drawSimpleBarChart(data) {
 
   // TODO: Cglass - Contact us
   // TODO: Cglass - My account - backend
-  // TOOD: Cglass - Continue History Page rework
+  // TOOD: Cglass - Continue History Page rework - filter controls
 }
