be190fc2e2f981e288e6f6c9e8fba768505280ff
julesdmai <julesdmai@gmail.com>
2025-10-21 16:23:13 -0500

    Cont CSS for signup - align with login

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -43,5 +43,5 @@ function drawSimpleBarChart(data) {
   // TODO: Research API GW
 
   // TODO: Cglass - Contact us
-  // TODO: Cglass - Sign Up page - css
+  // TODO: Cglass - Sign Up page - css (cont)
 }
