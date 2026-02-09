3c69f1bb619dc05f68f559878649b0958de1c615
julesdmai <julesdmai@gmail.com>
2026-02-08 18:58:38 -0800

    Finish Leave Group as Owner - Transfer Ownership

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -33,8 +33,7 @@ function drawSimpleBarChart(data) {
   // TODO: Stacked bar chart
   // TODO: Modify data set for stacked bar chart
   // TODO: Animation - Regular to stacked bar chart
-  
-  // TODO: Cglass - Groups - Leave Group as Owner - Transfer Ownership
+
   // TODO: Cglass - Groups - Disband Group
 
   // TODO: Cglass - Groups - Admin - Kick from Group
