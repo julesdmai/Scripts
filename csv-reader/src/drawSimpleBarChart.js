5fc12ce7394f6dd2bb99db5e2196ff993b44b86b
julesdmai <julesdmai@gmail.com>
2026-03-26 08:35:19 -0700

    Charts - 30d view

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -34,6 +34,7 @@ function drawSimpleBarChart(data) {
   // TODO: Animation - Regular to stacked bar chart
   
   // TODO: Cglass - Refactor - Time-based searches history page, recently deleted page, ...
-  // TODO: Cglass - 30d View
+  // TODO: Cglass - Charts - 30d View - State
+  // TODO: Cglass - Charts - 30d View - d3 Layers
 }
 
