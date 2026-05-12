76f9cd001a303dd5ed8cf7a68b9ec1db8cb48d33
julesdmai <julesdmai@gmail.com>
2026-05-11 22:41:38 -0700

    About

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -37,5 +37,6 @@ function drawSimpleBarChart(data) {
   // TODO: Cglass - Groups - Bootstrap API, Cleanup, Shared notes page, Animation (cont)
   // TODO: Cglass - Groups - PR 119
   // TODO: Cglass - Home Page - Shift Details saving locally Before/After Clock-In, Full Screen
+  // TODO: Cglass - About Page
   // TODO: Cglass - Monitoring + Alerting
 
