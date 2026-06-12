b08fc8c17f5a8dab47d15d421e439196fb530bb3
julesdmai <julesdmai@gmail.com>
2026-06-12 12:26:58 -0700

    Split backend / frontend for shared notes page

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -37,7 +37,8 @@ function drawSimpleBarChart(data) {
 
   // TODO: Cglass - Groups - Bootstrap API, Cleanup, 
   // TODO: Cglass - Groups - Bootstrap API - Frontend connection
-  // TODO: Cglass - Groups - Shared notes page - Backend, Frontend
+  // TODO: Cglass - Groups - Shared notes page - Backend
+  // TODO: Cglass - Groups - Shared notes page - Frontend
   // TODO: Cglass - Groups - Animation (cont)
   // TODO: Cglass - Groups - PR 119 - Submit
   // TODO: Cglass - Home Page - Shift Details saving locally Before/After Clock-In
