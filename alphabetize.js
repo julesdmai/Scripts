3b34c788600ee18a414ccb377974a9c704cc4ed5
julesdmai <julesdmai@gmail.com>
2026-06-24 11:20:09 -0700

    Added more executions

--- alphabetize.js
+++ alphabetize.js
@@ -6,14 +6,30 @@ function alphabetize(text) {
     .join("\n");
 }
 
-const sample = `  
-    draftFilters={draftFilters}
-    setDraftFilters={setDraftFilters}
-    applyFilters={applyFilters}
-    resetFilters={resetFilters}
-    canApply={canApply}
-    canReset={canReset}
-    openAdvFiltersModal={openAdvFiltersModal}
-    displayRange={displayRange}
-`;
-console.log(alphabetize(sample));
+// const sample = `  
+//     draftFilters={draftFilters}
+//     setDraftFilters={setDraftFilters}
+//     applyFilters={applyFilters}
+//     resetFilters={resetFilters}
+//     canApply={canApply}
+//     canReset={canReset}
+//     openAdvFiltersModal={openAdvFiltersModal}
+//     displayRange={displayRange}
+// `;
+// console.log(alphabetize(sample));
+
+const str = `
+    key={s.shift_id}
+    index={index}
+    shift_id={s.shift_id}
+    user_name={s.name}
+    member_number={s.member_number}
+    minutes_logged={s.minutes_logged}
+    start_time={s.start_time}
+    end_time={s.end_time}
+    project={s.project}
+    notes={s.notes}
+    openShiftDetailsModal={openShiftDetailsModal}
+`
+const results = alphabetize(str);
+console.log(results);
--- listToCheckStatementArray.js
+++ listToCheckStatementArray.js
@@ -51,8 +51,62 @@ function listToCheckStatementArray(str, lbl) {
 // const results = listToCheckStatementArray(options, label);
 // console.log(results);
 
+// const options =
+//   "American Indian or Alaska Native | Asian | Black or African American | Native Hawaiian or Other Pacific Islander | White | Two or more races | Some other race | Prefer not to say";
+// const label = "race";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Actor | Model | Singer | Dancer | Athlete | Presenter | Influencer | Other";
+// const label = "primary_discipline";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Drama | Comedy | Commercial | Improv | Voice Over | Motion Capture | Theatre | Soap Opera | Other";
+// const label = "acting_genres";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Runway | Commercial | Editorial | Fitness | Parts | Petite | Plus-Size | Swimwear | Other";
+// const label = "modeling_categories";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Ballet | Jazz | Hip Hop | Contemporary | Ballroom | Latin | Tap | Salsa | Breakdancing | Other";
+// const label = "dance_styles";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Film | Television | Commercials | Print Modeling | Runway | Brand Partnerships | UGC Content | Voiceover | Music Opportunities | Public Speaking | Other";
+// const label = "opportunities_sought";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Acting & Performance | Comedy | Fashion | Beauty | Fitness | Wellness | Lifestyle | Travel | Music | Entrepreneurship | Education | Family | Gaming | Food | Technology | Outdoor Adventure | Luxury | Faith | Other";
+// const label = "brand_pillars";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Casting Directors | Producers | Brands | Modeling Agencies | Talent Agencies | Fans | Industry Professionals | Businesses | Other";
+// const label = "target_audience";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
+// const options =
+//   "Instagram | TikTok | Facebook | LinkedIn | YouTube | IMDb | Personal Website | Other";
+// const label = "active_platforms";
+// const results = listToCheckStatementArray(options, label);
+// console.log(results);
+
 const options =
-  "American Indian or Alaska Native | Asian | Black or African American | Native Hawaiian or Other Pacific Islander | White | Two or more races | Some other race | Prefer not to say";
-const label = "race";
+  "Short-form video | Long-form video | UGC | Live streaming | Podcast | Editorial / print | Behind-the-scenes | Tutorials | Interviews | Other";
+const label = "content_format_comfort";
 const results = listToCheckStatementArray(options, label);
 console.log(results);
