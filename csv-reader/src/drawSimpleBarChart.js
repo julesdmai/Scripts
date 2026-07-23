2b8e59184e9e02f0c2238e746ab171bdcbecd610
julesdmai <julesdmai@gmail.com>
2026-07-22 18:13:34 -0700

    simplified divider borders

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -46,10 +46,6 @@ function drawSimpleBarChart(data) {
 
   // TODO: Nuvue - About / Contact (Paused)
 
-  // TODO: Nuvue - Talents - Admin Talent List - Rows
-  // TODO: Nuvue - Talents - Admin Talent List - Edit
-  // TODO: Nuvue - Talents - Admin Talent List - Visibility
-
   // TODO: Nuvue - Talents - Admin Talent List - Thumbnail Media
 
   // TODO: Nuvue - DB Diagram
