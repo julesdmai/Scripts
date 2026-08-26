210beb62551c9a221d11e242a3e472f44910c6b6
julesdmai <julesdmai@gmail.com>
2026-08-26 10:12:42 -0700

    og images

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -44,7 +44,7 @@ function drawSimpleBarChart(data) {
 
   // TODO: Cglass - Monitoring + Alerting
 
-  // TODO: Nuvue - SEO - OG images
+  // TODO: Nuvue - SEO - OG images (cont)
   // TODO: Nuvue - SEO - sitemap (cont)
 
   // TODO: Nuvue - About Page (Matt)
