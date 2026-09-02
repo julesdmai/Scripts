0d9c683ef2d8e76c8a60a92ad6f61df6d63ae6a6
julesdmai <julesdmai@gmail.com>
2026-09-02 10:37:25 -0700

    favicon

--- csv-reader/src/drawSimpleBarChart.js
+++ csv-reader/src/drawSimpleBarChart.js
@@ -44,9 +44,8 @@ function drawSimpleBarChart(data) {
 
   // TODO: Cglass - Monitoring + Alerting
 
-  // TODO: Nuvue - SEO - meta tags (cont)
-  // TODO: Nuvue - SEO - OG images (test)
   // TODO: Nuvue - SEO - sitemap
+  // TODO: Nuvue - favicon
 
   // TODO: Nuvue - About Page (Matt)
   // TODO: Nuvue - Contact (Jacob)
